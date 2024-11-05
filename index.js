87b88b0979f2e5d86691288ef76f5e72a1cce58c
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-11-05 08:41:29 +0200

    pug

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-import getJson from './js/getJson.js';
-import renderTable from './js/renderTable.js';
 
-const table = document.querySelector('tbody');
-
-const data = await getJson('/data/table.json');
-
-const markup = renderTable(data);
-
-table.innerHTML = markup;
--- js/getJson.js
+++
@@ -1,5 +0,0 @@
-export default async function getJson(url) {
-  const data = await fetch(url);
-  const json = await data.json();
-  return json;
-}
