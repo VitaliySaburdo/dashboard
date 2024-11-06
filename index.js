7dc3429dc3f9c8ebd8f7087111392d471393bed4
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-11-06 12:07:19 +0200

    add menu

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const burgerMenu = document.querySelector('#burger-menu');
+const mobilMenu = document.querySelector('#mobile-menu');
 
+console.log(burgerMenu);
+console.log(mobilMenu);
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('active');
+  mobilMenu.classList.toggle('active');
+});
